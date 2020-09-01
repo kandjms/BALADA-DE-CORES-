305ad60ba142e9cec163dad7cf6e09ebe743876b
João Vítor 2 <70592775+kandjms@users.noreply.github.com>
2020-09-01 14:05:06 -0300

    Add files via upload

---
+++ LISTA.cmd
@@ -0,0 +1,31 @@
+@echo  off
+title balada?, de cores!
+prompt balada?, de cores!
+ :loop
+color 1
+color 2
+color 3 
+color 4
+color 5 
+color 6
+color 7
+color 8
+color 9
+goto loop
+cls
+cmd
+cls 
+set w=oia
+echo %w%
+:w
+echo oi,tudo bem??
+goto w
+set quit=exit
+:quit
+echo 1000
+goto quit 
+ :quit
+ %quit%
+ goto quit.-.-.
+ %quit%
+ 
